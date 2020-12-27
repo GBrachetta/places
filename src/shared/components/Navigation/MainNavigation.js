0674dd2e984643a0e8c3326d9c4426b5d083a063
Guillermo Brachetta <brachetta@me.com>
2020-12-27 12:36:00 +0100

    Add SideDrawer component and render it with portal. Add NavLinks component

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import MainHeader from './MainHeader';
 
 import './MainNavigation.css';
+import NavLInks from './NavLInks';
+import SideDrawer from './SideDrawer';
 
 const MainNavigation = props => {
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">Your Places</Link>
-      </h1>
-      <nav>...</nav>
-    </MainHeader>
+    <Fragment>
+      <SideDrawer>
+        <nav className="main-navigation__drawer-nav">
+          <NavLInks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className="main-navigation__menu-btn">
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">Your Places</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLInks />
+        </nav>
+      </MainHeader>
+    </Fragment>
   );
 };
 
